465a56e4fc0f30813cc60dc15183ec57db6a6784
mixereqv1 <mateuszral7@gmail.com>
2020-11-04 21:01:35 +0100

    adding main page template

---
+++ src/templates/MainTemplate.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from 'assets/styles/globalStyles';
+import { theme } from 'assets/styles/theme';
+
+import Navigation from 'components/Navigation/Navigation';
+
+const MainTemplate = ({ children }) => {
+  return (
+    <div>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Navigation />
+        {children}
+      </ThemeProvider>
+    </div>
+  );
+};
+
+MainTemplate.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default MainTemplate;
